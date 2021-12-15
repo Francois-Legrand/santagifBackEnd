a5e069c0dc3a9e0e474502bdb5b9ce003d4de267
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-12-15 14:50:57 +0100

    first commit

---
+++ src/main/java/com/example/santagifbackend/SantaGifBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.santagifbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaGifBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantaGifBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/santagifbackend/SantaGifBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.santagifbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaGifBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
